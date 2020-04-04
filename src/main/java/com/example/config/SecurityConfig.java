5dbc24a458693f863d6b3b567c587ddc53335ce6
Administrator <chenheng0830@gmail.com>
2020-04-04 12:09:46 +0800

    add user and get user method

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .anyRequest()
                 .permitAll()
                 .and().csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,18 +1,33 @@
 package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
-    @Value("${spring.application.name}")
-    String appName;
 
-    @RequestMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "index";
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/user")
+    public List<User> getUser(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping(value = "/user")
+    public User createUser(
+            @RequestParam String userName,
+            @RequestParam String password){
+        User user =new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        return userRepository.save(user);
     }
 }
